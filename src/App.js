e3cb9d5849141f3c5e62d3c218d6f9724fd13a66
John Gamboa <gamboa.john@protonmail.com>
2022-01-27 19:00:14 +0800

    updated index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import './styles/App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <h1>fdsfdsfd</h1>
+      <i>fdsfdsf</i>
+      <p>fsdfdsfd</p>
     </div>
   );
 };
