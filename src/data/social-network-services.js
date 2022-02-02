1fcf9d62bd8a9870be82efaa1586f66f87ca84a6
John Gamboa <gamboa.john@protonmail.com>
2022-02-02 10:12:40 +0800

    updated social-network-services.js, deleted its json file

--- src/data/social-network-services.js
+++ src/data/social-network-services.js
@@ -2,6 +2,28 @@ import discord from '../img/socials/discord.svg';
 import facebook from '../img/socials/facebook.svg';
 import facebookMessenger from '../img/socials/facebook-messenger.svg';
 import flickr from '../img/socials/flickr.svg';
+import instagram from '../img/socials/instagram.svg';
+import line from '../img/socials/line.svg';
+import linkedin from '../img/socials/linkedin.svg';
+import pinterest from '../img/socials/pinterest.svg';
+import qq from '../img/socials/qq.svg';
+import quora from '../img/socials/quora.svg';
+import reddit from '../img/socials/reddit.svg';
+import skype from '../img/socials/skype.svg';
+import slack from '../img/socials/slack.svg';
+import snapchat from '../img/socials/snapchat.svg';
+import stackOverflow from '../img/socials/stack-overflow.svg';
+import telegram from '../img/socials/telegram.svg';
+import tiktok from '../img/socials/tiktok.svg';
+import tinder from '../img/socials/tinder.svg';
+import tumblr from '../img/socials/tumblr.svg';
+import twitch from '../img/socials/twitch.svg';
+import twitter from '../img/socials/twitter.svg';
+import viber from '../img/socials/viber.svg';
+import wechat from '../img/socials/wechat.svg';
+import weibo from '../img/socials/weibo.svg';
+import whatsapp from '../img/socials/whatsapp.svg';
+import youtube from '../img/socials/youtube.svg';
 
 export const sns = [
   {
@@ -35,154 +57,154 @@ export const sns = [
   {
     "id": "sns05",
     "name": "Instagram",
-    "src": "./img/instagram.svg",
+    "src": instagram,
     "alt": "Instagram logo",
     "clicked": false
   },
   {
     "id": "sns06",
     "name": "Line",
-    "src": "./img/line.svg",
+    "src": line,
     "alt": "Line logo",
     "clicked": false
   },
   {
     "id": "sns07",
     "name": "LinkedIn",
-    "src": "./img/linkedin.svg",
+    "src": linkedin,
     "alt": "LinkedIn logo",
     "clicked": false
   },
   {
     "id": "sns08",
     "name": "Pinterest",
-    "src": "./img/pinterest.svg",
+    "src": pinterest,
     "alt": "Pinterest logo",
     "clicked": false
   },
   {
     "id": "sns09",
     "name": "QQ",
-    "src": "./img/qq.svg",
+    "src": qq,
     "alt": "QQ logo",
     "clicked": false
   },
   {
     "id": "sns10",
     "name": "Quora",
-    "src": "./img/quora.svg",
+    "src": quora,
     "alt": "Quora logo",
     "clicked": false
   },
   {
     "id": "sns11",
     "name": "Reddit",
-    "src": "./img/reddit.svg",
+    "src": reddit,
     "alt": "Reddit logo",
     "clicked": false
   },
   {
     "id": "sns12",
     "name": "Skype",
-    "src": "./img/skype.svg",
+    "src": skype,
     "alt": "Skype logo",
     "clicked": false
   },
   {
     "id": "sns13",
     "name": "Slack",
-    "src": "./img/slack.svg",
+    "src": slack,
     "alt": "Slack logo",
     "clicked": false
   },
   {
     "id": "sns14",
     "name": "Snapchat",
-    "src": "./img/snapchat.svg",
+    "src": snapchat,
     "alt": "Snapchat logo",
     "clicked": false
   },
   {
     "id": "sns15",
     "name": "Stack Overflow",
-    "src": "./img/stack-overflow.svg",
+    "src": stackOverflow,
     "alt": "Stack Overflow logo",
     "clicked": false
   },
   {
     "id": "sns16",
     "name": "Telegram",
-    "src": "./img/telegram.svg",
+    "src": telegram,
     "alt": "Telegram logo",
     "clicked": false
   },
   {
     "id": "sns17",
     "name": "TikTok",
-    "src": "./img/tiktok.svg",
+    "src": tiktok,
     "alt": "TikTok logo",
     "clicked": false
   },
   {
     "id": "sns18",
     "name": "Tinder",
-    "src": "./img/tinder.svg",
+    "src": tinder,
     "alt": "Tinder logo",
     "clicked": false
   },
   {
     "id": "sns19",
     "name": "Tumblr",
-    "src": "./img/tumblr.svg",
+    "src": tumblr,
     "alt": "Tumblr logo",
     "clicked": false
   },
   {
     "id": "sns20",
     "name": "Twitch",
-    "src": "./img/twitch.svg",
+    "src": twitch,
     "alt": "Twitch logo",
     "clicked": false
   },
   {
     "id": "sns21",
     "name": "Twitter",
-    "src": "./img/twitter.svg",
+    "src": twitter,
     "alt": "Twitter logo",
     "clicked": false
   },
   {
     "id": "sns22",
     "name": "Viber",
-    "src": "./img/viber.svg",
+    "src": viber,
     "alt": "Viber logo",
     "clicked": false
   },
   {
     "id": "sns23",
     "name": "WeChat",
-    "src": "./img/wechat.svg",
+    "src": wechat,
     "alt": "WeChat logo",
     "clicked": false
   },
   {
     "id": "sns24",
     "name": "Weibo",
-    "src": "./img/weibo.svg",
+    "src": weibo,
     "alt": "Weibo logo",
     "clicked": false
   },
   {
     "id": "sns25",
     "name": "WhatsApp",
-    "src": "./img/whatsapp.svg",
+    "src": whatsapp,
     "alt": "WhatsApp logo",
     "clicked": false
   },
   {
     "id": "sns26",
     "name": "Youtube",
-    "src": "./img/youtube.svg",
+    "src": youtube,
     "alt": "Youtube logo",
     "clicked": false
   }
